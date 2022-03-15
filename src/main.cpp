06b7ca18c3a07203d4bf493e7bf814fea5c7a1fc
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-03-15 11:31:34 -0700

    Terminal Interrupt Handling
    The program will now gracefully shutdown when control + c is pressed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "ztnode.hpp"
+#include <csignal>
+
+// Variable storing our connection to ZeroTier
+ZeroTierNode node;
+
+// Callback that shuts down the program when interupted (ctrl + c in terminal)
+void signalCallbackHandler(int signum) {
+	// Terminate program
+	std::exit(signum);
+}
 
 int main() {
-	ZeroTierNode node;
+	// Gracefully terminate when interupted
+	signal(SIGINT, signalCallbackHandler);
+
+	// Establish our connection to ZeroTier
 	node.setup();
 
 	std::cout << "\nConnection IP: >> " << node.getIP() << " <<\n" << std::endl;
